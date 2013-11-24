a1b64fb07ff3009cce6ad919bafc1f1444a00b4f
Danil Ozherelyev <ozherelyev@gmail.com>
2013-11-24 11:27:25 -0800

    hibernate cyrillic fix attempt

--- src/main/java/com/seaport/init/RootConfig.java
+++ src/main/java/com/seaport/init/RootConfig.java
@@ -64,8 +64,11 @@ public class RootConfig {
 		properties.setProperty("hibernate.format_sql", "true");
 		properties.setProperty("hibernate.default_schema", env.getProperty("hibernate.default.schema"));
 		properties.setProperty("hibernate.generate_statistics", "true");
-		properties.setProperty("hibernate.connection.datasource", env.getProperty("jndi.name"));		
+		properties.setProperty("hibernate.connection.datasource", env.getProperty("jndi.name"));
 		
+		properties.setProperty("hibernate.connection.useUnicode", "true");
+		properties.setProperty("hibernate.connection.characterEncoding", "UTF-8");
+		properties.setProperty("hibernate.connection.charSet", "UTF-8");
 		return properties;	
 	}
 	
