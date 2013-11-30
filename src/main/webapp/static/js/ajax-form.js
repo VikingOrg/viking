ad4f39d0631a67ca7a41c3f6b8366b1db28dd255
Danil Ozherelyev <ozherelyev@gmail.com>
2013-11-29 20:49:59 -0800

    Ajax call

--- src/main/webapp/static/js/ajax-form.js
+++ src/main/webapp/static/js/ajax-form.js
@@ -1,18 +1,70 @@
+
 jQuery(function($) {
-		$('form[data-async]').on('submit', function(event) {
-				var $form = $(this);
-				var $target = $($form.attr('data-target'));
-				 
-				$.ajax({
-						type: $form.attr('method'),
-						url: $form.attr('action'),
-						data: $form.serialize(),
-						 
-						success: function(data, status) {
-								$target.html(data);
-						}
-				});
-				 
-				event.preventDefault();
-		});
+	$('form[data-async]').on('submit', function(event) {
+		var $form = $(this);
+		var $target = $($form.attr('data-target'));
+		var action =  $form.attr('action');
+		var method =  $form.attr('method');
+        $.ajax({
+        	type: "POST",
+		    url: "userSearchAdmin",
+		    data: $('form.contact').serialize(),
+	        success: function(msg){
+	             $("#thanks").html(msg)
+	             $("#form-content").modal('hide');    
+	        },
+	        error: function(){
+	        	alert("failure");
+	        }
+         });
+		 
+		event.preventDefault();
+	});
 });
+
+
+//jQuery(function($) {
+//	$('form[data-async]').on('submit', function(event) {
+//		var $form = $(this);
+//		var $target = $($form.attr('data-target'));
+//		var action =  $form.attr('action');
+//		var method =  $form.attr('method');
+//		$.ajax({
+//			type: $form.attr('method'),
+//			url: $form.attr('action'),
+//			data: $('form.contact').serialize(),
+//			 
+//            success: function(msg){
+//                $("#thanks").html(msg)
+//                $("#form-content").modal('hide');    
+//            },
+//			
+//	        error: function(){
+//	            alert("failure");
+//	        }
+//		
+//		});
+//		 
+//		event.preventDefault();
+//	});
+//});
+
+
+//$(function() {
+//	//twitter bootstrap script
+//	    $("button#submit").click(function(){
+//	               $.ajax({
+//	                   type: "POST",
+//	            url: "process.php",
+//	            data: $('form.contact').serialize(),
+//	                success: function(msg){
+//	                        $("#thanks").html(msg)
+//	                    $("#form-content").modal('hide');    
+//	                },
+//	            error: function(){
+//	                alert("failure");
+//	                }
+//	                  });
+//	    });
+//	});
+
