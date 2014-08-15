404efdaccfc9c48cba6e829f7424a30be2b9489e
Danil Ozherelyev <ozherelyev@gmail.com>
2014-08-14 22:22:06 -0700

    ..minor check in.

---
+++ src/main/java/com/seaport/dao/IReportDAO.java
@@ -0,0 +1,13 @@
+package com.seaport.dao;
+
+
+/**
+ * The DAO interface for Group requests
+ * 
+ * @Author Danil Ozherelyev
+ * @version 1.0 08/12/14
+ *          
+ */
+public interface IReportDAO {
+
+}
